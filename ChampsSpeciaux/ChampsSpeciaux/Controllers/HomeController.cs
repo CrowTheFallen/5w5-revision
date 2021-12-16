07a61026ca3eed07aa0815ff4c4219cccde40d58
Alex Blanchette <bigue1994@hotmail.com>
2021-12-16 15:36:20 -0500

    Ajout icone, champ de text, ajout d'image, gestion erreur(toast)

--- ChampsSpeciaux/ChampsSpeciaux/Controllers/HomeController.cs
+++ ChampsSpeciaux/ChampsSpeciaux/Controllers/HomeController.cs
@@ -8,6 +8,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using ChampsSpeciaux.Utility;
+using System.IO;
 
 namespace ChampsSpeciaux.Controllers
 {
@@ -15,11 +18,13 @@ namespace ChampsSpeciaux.Controllers
     {
         private readonly ILogger<HomeController> _logger; 
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public async Task<IActionResult> Index()
@@ -41,12 +46,34 @@ namespace ChampsSpeciaux.Controllers
         {
             if (ModelState.IsValid)
             {
+                string webRootPath = _webHostEnvironment.WebRootPath; //Chemin des images de voyage: Travel
+                var files = HttpContext.Request.Form.Files; //nouvelle image récupérée
+
+                if (files.Count > 0)
+                {
+                    string fileName = Guid.NewGuid().ToString();// Nom fichier généré, unique
+                    var uploads = Path.Combine(webRootPath, AppConstants.ImagePath);// chemin pour les image
+                    var extenstion = Path.GetExtension(files[0].FileName); // extraire l'extention du fichier
+
+                    // Create un cannal pour transférer le fichier 
+                    using (var filesStreams = new FileStream(Path.Combine("..\\ChampsSpeciaux\\wwwroot\\images\\", fileName + extenstion), FileMode.Create))
+                    {
+                        files[0].CopyTo(filesStreams);
+                    }
+
+                    // Composer le nom du fichier avec son extension qui sera enregister dans la BD
+                    // avec le path relatif à partir du Root
+                    // sans le path relatif (le path devra être ajouté dans la View)
+                    travel.TravelImage = fileName + extenstion;
+                }
+
                 // Ajouter à la BD
                 await _unitOfWork.Travel.AddAsync(travel);
-
+                TempData[AppConstants.Success] = "The travel was created.";
                 _unitOfWork.Save();
                 return RedirectToAction(nameof(Index));
             }
+            TempData[AppConstants.Error] = "An error occurred while creating the Travel.";
             return this.View(travel);
         }
 
@@ -65,17 +92,62 @@ namespace ChampsSpeciaux.Controllers
         //POST - EDIT
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(Travel travel)
+        public async Task<IActionResult> Edit(Travel travel)
         {
             if (ModelState.IsValid)
             {
+                string webRootPath = _webHostEnvironment.WebRootPath; //Chemin des images de voyage: Travel
+                var files = HttpContext.Request.Form.Files; //nouvelle image récupérée
+
+                if (files.Count > 0)
+                {
+                    string fileName = Guid.NewGuid().ToString();// Nom fichier généré, unique
+                    var uploads = Path.Combine(webRootPath, AppConstants.ImagePath);// chemin pour les image
+                    var extenstion = Path.GetExtension(files[0].FileName); // extraire l'extention du fichier
+
+
+                    if (travel.TravelImage != null)
+                    {
+                        //L'image est modifiée: l'ancienne doit être supprimée
+                        var imagePath = Path.Combine(webRootPath, travel.TravelImage.TrimStart('\\'));
+                        if (System.IO.File.Exists(imagePath))
+                        {
+                            System.IO.File.Delete(imagePath);
+                        }
+                    }
+                   
+
+                    // Create un canal pour transférer le fichier 
+                    using (var filesStreams = new FileStream(Path.Combine("..\\ChampsSpeciaux\\wwwroot\\images\\", fileName + extenstion), FileMode.Create))
+                    {
+                        files[0].CopyTo(filesStreams);
+                    }
+                    // Composer le nom du fichier avec son extension qui sera enregister dans la BD
+                    // avec le path relatif à partir du Root
+                    // sans le path relatif (le path devra être ajouté dans la View)
+                    travel.TravelImage = fileName + extenstion;
+                }
+                else
+                {
+                    // L'image n'est pas modifiée: on récupère l'information
+                    // afin qu'elle ne soit pas perdue après l'update
+                    Travel objFromDb = await _unitOfWork.Travel.GetAsync(travel.Id);
+                    travel.TravelImage = objFromDb.TravelImage;
+
+                }
+
                 _unitOfWork.Travel.Update(travel);
+                TempData[AppConstants.Success] = "The travel was updated.";
                 _unitOfWork.Save();
+
                 return RedirectToAction(nameof(Index));
             }
-            return View(travel);
+            else
+            {
+                TempData[AppConstants.Error] = "An error occurred while updating the Travel.";
+                return View(travel);
+            }
         }
-
 
         public IActionResult Privacy()
         {
@@ -89,3 +161,4 @@ namespace ChampsSpeciaux.Controllers
         }
     }
 }
+    
---
+++ ChampsSpeciaux/ChampsSpeciaux/Utility/AppConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChampsSpeciaux.Utility
+{
+    public class AppConstants
+    {
+        public static string ImagePath = @"\images\";
+        public static string ImagePathView = @"/images/";
+        public static string ImageGeneric = "TravelGeneric.png";
+
+        public static string Success = "Succès!";
+        public static string Error = "Error -_-";
+        
+    }
+}
